af694f9aeb478256df3a3607692aa4ab940e49b6
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-21 10:56:37 +0000

    automatically backed up by learn

--- db/migrate/007_add_genre_to_show.rb
+++ db/migrate/007_add_genre_to_show.rb
@@ -1,6 +1,5 @@
-add_genre_to_show
-class AddCatchphraseToCharacter < ActiveRecord::Migration[5.1]
+class AddGenreToShow < ActiveRecord::Migration[5.1]
   def change 
-    add_column :characters, :catchphrase, :string
+    add_column :shows, :genre, :string
   end 
 end 
