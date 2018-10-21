979f84f7907ac3b98dc4879d34cd2bdd341e6691
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-10-21 10:48:13 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,6 +1,6 @@
 class CreateCharacters < ActiveRecord::Migration[5.1]
   def change 
-    crate_table :characters do |t|
+    create_table :characters do |t|
       t.string :name
       t.string :show_id
     end 
